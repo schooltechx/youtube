b259174b64c882fbf3dd0c3d1316691dd2cec4be
schooltechx <99984575+schooltechx@users.noreply.github.com>
2022-07-17 20:27:37 +0700

    Create Pizza.cs

---
+++ webapp/dotnet-minimal-api-ef-15m/Models/Pizza.cs
@@ -0,0 +1,9 @@
+namespace PizzaStore.Models 
+{
+  public class Pizza
+  {
+      public int Id { get; set; }
+      public string? Name { get; set; }
+      public string? Description { get; set; }
+  }
+}
