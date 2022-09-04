a070c8a541d39bec3fe7f89d9b5fd720d1841071
schooltechx <99984575+schooltechx@users.noreply.github.com>
2022-09-05 01:16:46 +0700

    Create +page.svelte

---
+++ svelte/svelte-kit/sveltekit-fullstack/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import {PUBLIC_VAR} from  '$env/static/public'
+    /** @type {import('./$types').PageData} */
+    export let data
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+
+<h1>{data.title}</h1>
+<div>{@html data.content}</div>
+<div>{PUBLIC_VAR} </div>
