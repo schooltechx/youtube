e4a77e71a37199d310dc0b0c872c8cbda9b81f57
schooltechx <99984575+schooltechx@users.noreply.github.com>
2022-09-06 08:55:28 +0700

    Delete +page.server.js

--- svelte/svelte-kit/sveltekit-fullstack/src/+page.server.js
+++
@@ -1,9 +0,0 @@
-import {SOME_VALUE} from  '$env/static/private';
-/** @type {import('./$types').PageServerLoad} */
-export function load({ url }) {
-    let who= url.searchParams.get("who") ?? 'guest'
-    return {
-        title: 'Hello ' + who,
-        content: '<b>Welcome</b> to our blog.'+SOME_VALUE
-    };
-}
