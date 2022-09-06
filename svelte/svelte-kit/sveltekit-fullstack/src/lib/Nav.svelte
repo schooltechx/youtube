d0b8371e8c5277fa95eb5cdfff4f58d60c63389f
schooltechx <99984575+schooltechx@users.noreply.github.com>
2022-09-06 08:55:46 +0700

    Add files via upload

---
+++ svelte/svelte-kit/sveltekit-fullstack/src/lib/Nav.svelte
@@ -0,0 +1,22 @@
+<script>
+    import {
+      Header,
+      SideNav,
+      SideNavItems,
+      SideNavLink,
+    } from "carbon-components-svelte";
+  
+    let isSideNavOpen = false;
+  </script>
+  
+  <Header company="SvelteKit" platformName="Demo" bind:isSideNavOpen>
+  </Header>
+  
+  <SideNav bind:isOpen={isSideNavOpen}>
+    <SideNavItems>
+      <SideNavLink href="/" text="Home" />
+      <SideNavLink href="/user" text="User" />
+      <SideNavLink href="/about" text="About" />
+    </SideNavItems>
+  </SideNav>
+  
