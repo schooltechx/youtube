ff066e5bdec3aa1550613754450bb94e560dece5
schooltechx <99984575+schooltechx@users.noreply.github.com>
2022-09-06 08:49:29 +0700

    Add files via upload

---
+++ svelte/svelte-kit/sveltekit-fullstack/src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script>
+  import "carbon-components-svelte/css/white.css";
+
+  import Nav from "$lib/Nav.svelte"
+  import {
+      Content,
+      Grid,
+      Row,
+      Column,
+    } from "carbon-components-svelte";
+
+</script>
+  <Nav/>
+  <Content>
+    <Grid>
+      <Row>
+        <Column>
+          <slot></slot>
+        </Column>
+      </Row>
+    </Grid>
+  </Content>
+  
+  <hr>
+  by Oom
+  
