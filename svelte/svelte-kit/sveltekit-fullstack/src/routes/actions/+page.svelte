c0f726c18578f848cbeb1e7fb270ccc26e3f195f
schooltechx <99984575+schooltechx@users.noreply.github.com>
2022-09-12 23:01:20 +0700

    Update +page.svelte

--- svelte/svelte-kit/sveltekit-fullstack/src/routes/actions/+page.svelte
+++ svelte/svelte-kit/sveltekit-fullstack/src/routes/actions/+page.svelte
@@ -1,20 +1,22 @@
 <script>
-    import { enhance } from '$app/forms'
-    import { page } from '$app/stores';
+// @ts-nocheck
 
-    import {Form,TextInput,Button} from "carbon-components-svelte";
+	import { enhance } from '$app/forms';
+	import { TextInput,Form,Button } from "carbon-components-svelte";
 	/** @type {import('./$types').PageData} */
 	export let data
-    /** @type {import('./$types').ActionData} */
-    export let form;
-    /** @type {{message:string}} */
+	/** @type {import('./$types').ActionData} */
+	export let form;
+
+	/** @type {{message:string}} */
 	export let errors
-    let id=0,name="",email=""
+	let id=0,name="",email=""
 </script>
+
 {#if form?.message}
-	{#if form.success} <div>เพิ่มหรืออัปเดตยูสเซอร์สำเร็จ</div>{/if}
-	{#if form.missing} <div>ข้อมูลไม่ครบ</div> {/if}
-	{#if form.incorrect} <div>มีบางอย่างไม่ถูกต้อง</div> {/if}
+	{#if form?.success} <div>เพิ่มยูสเซอร์สำเร็จ</div>{/if}
+	{#if form?.missing} <div>ข้อมูลไม่ครบ</div> {/if}
+	{#if form?.incorrect} <div>มีบางอย่างไม่ถูกต้อง</div> {/if}
   	<p>{form.message}</p>
 {/if}
 
@@ -26,31 +28,30 @@
 	<button formaction="?/update">update</button>
 </form>
 
-<Form method="POST">
-    <TextInput hidden name="id" bind:value={id} />
-    <TextInput name="name" bind:value={name} labelText="User name" placeholder="Enter user name..." />
-    <TextInput name="email" bind:value={email} labelText="User name" placeholder="Enter Email..." />
-    <Button type="submit" size="small" on:click={()=>id=0} formaction="?/create" 
-    disabled={name==""||email==""} >
-        Add
-    </Button>
-    <Button type="submit" size="small" formaction="?/update" disabled={id==0|| name==""||email==""}>Update</Button>
+<Form method="POST" >
+	<TextInput hidden name="id" bind:value={id} />
+	<TextInput name="name" bind:value={name} required placeholder="Enter user name..." />
+	<TextInput name="email" bind:value={email} required placeholder="Enter Email..." />
+	<Button type="submit" size="small" formaction="?/create" 
+		on:click={()=>id=0} disabled={name==""||email==""}>Add</Button>
+	<Button type="submit" size="small" formaction="?/update" 
+		disabled={id==0||name==""||email==""}>Update</Button>
 </Form>
 {#if errors}
 <b>{errors.message}</b>
 {/if}
-<h1>User List</h1>
+<h1>User list</h1>
 <ul>
-    {#each data.users as user}
-    <li>
-        <a href="/actions?del={user.id}">x</a>
-        <b on:click={()=>{
-            id=user.id
-            name=user.name
-            email=user.email}}>
-            {user.name}:{user.email}
-        </b>
-    </li>
-    {/each}
+	{#each data.users as user}
+		<li>
+			<a href="/user?del={user.id}">x</a>
+			<b on:click={()=>{
+					id=user.id 
+					name=user.name??""
+					email=user.email
+                }}>
+                {user.name}: {user.email}
+            </b>	
+		</li>
+	{/each}
 </ul>
-{$page.status}
