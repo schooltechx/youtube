451f09a47b2a5ba353079f6b48f84d804c39c9fb
oom <99984575+schooltechx@users.noreply.github.com>
2022-11-11 18:38:56 +0700

    Update and rename load-users to load-users.svelte

--- classroom/self-improvement-classroom/fetch/load-users.svelte
+++ classroom/self-improvement-classroom/fetch/load-users.svelte
@@ -1,10 +1,10 @@
 <script>
   //code for App.svelte
   let users=[]
-	async function load(){
-		const res = await fetch('https://jsonplaceholder.typicode.com/users');
-		users = await res.json();
-	}
+  async function load(){
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    users = await res.json();
+  }
 </script>
 <button on:click={load}>Load Users</button>
 {#each users as user}
