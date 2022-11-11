af338429fb5c1d1b926658d5ba78817a1f86437e
oom <99984575+schooltechx@users.noreply.github.com>
2022-11-11 18:36:53 +0700

    Update load-pokemon.svelte

--- classroom/self-improvement-classroom/fetch/load-pokemon.svelte
+++ classroom/self-improvement-classroom/fetch/load-pokemon.svelte
@@ -1,17 +1,17 @@
 <script>
   //code for App.svelte
-  let mon
-	let name="ditto"
-	async function load(){
-		const res = await fetch('https://pokeapi.co/api/v2/pokemon/'+name);
-		mon = await res.json();
-	}
+let mon
+let name="ditto"
+async function load(){
+  const res = await fetch('https://pokeapi.co/api/v2/pokemon/'+name);
+  mon = await res.json();
+}
 </script>
+<input bind:value={name}>
+<button on:click={load}>Load</button>
 <ol>
-	<input bind:value={name}>
-	<button on:click={load}>Load</button>
-	{#if mon}
+  {#if mon}
     <li>Name: {mon.name}</li>
     <li>Weight: {mon.weight}</li>
-	{/if}
+  {/if}
 </ol>
