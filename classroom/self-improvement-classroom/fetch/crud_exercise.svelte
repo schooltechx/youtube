104d85f16621fe339498c19251c9338560c3925a
oom <72376671+sorawit-frappet@users.noreply.github.com>
2022-11-25 18:39:34 +0700

    add line notify

--- classroom/self-improvement-classroom/fetch/crud_exercise.svelte
+++ classroom/self-improvement-classroom/fetch/crud_exercise.svelte
@@ -6,53 +6,64 @@
   // https://www.w3schools.com/jsref/jsref_find.asp
   // https://www.youtube.com/watch?v=Q_fN0MQiBs4
 
-  import { onMount } from 'svelte';
-   let posts=[],title="",author="",id=0,msg=""
-   onMount(load);
-   async function load(){
-     const res = await fetch('http://localhost:3000/posts')
-     posts = await res.json();
-     if (res.status != 200) {
-       msg = 'Can not load post list'
-     }
-   }
-   async function create(){
-     console.log({ title, author })
-     const res = await fetch('http://localhost:3000/posts', {
-       method: 'POST',
-       headers: {'Content-Type': 'application/json'},
-       body: JSON.stringify({title,author})
-     });
-     if (!res.ok) {
-       msg = 'create user fail '+res.status
-     }
-     load()
- }
- async function del(sel_id){
-   msg= "I wan to delete user id "+sel_id
- }
- async function sel(sel_id){
-   let p  = posts.find((o)=>o.id==sel_id)
-   if(p){
-     id= p.id
-     title=p.title
-     author=p.author
-     msg=`Post ${id} selected`
-   }
- }
- </script>
- <div>
-   <input placeholder="Title" bind:value={title}>
-   <input placeholder="Author" bind:value={author}>  
-   <button on:click={create} disabled={!title||!author}>Create</button>
-   <button on:click={load} disabled={!title||!author||id==0}>Update {id}</button>
- </div>
- {#each posts as post}
-   <div>
-     <input type="radio" name="po" on:click={()=>sel(post.id)}>
-     <button on:click={()=>del(post.id)}>x</button>  
-     {post.title}({post.author}) 
-   </div>
- {/each}
- <div>{msg}</div>
- 
+  import { onMount } from "svelte"
+  let posts = [],
+    title = "",
+    author = "",
+    id = 0,
+    msg = ""
+  onMount(load)
+  async function load() {
+    const res = await fetch("http://localhost:3000/posts")
+    posts = await res.json()
+    if (res.status != 200) {
+      msg = "Can not load post list"
+    }
+  }
+  async function create() {
+    console.log({ title, author })
+    const res = await fetch("http://localhost:3000/posts", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title, author }),
+    })
+    if (!res.ok) {
+      msg = "create user fail " + res.status
+    }
+    load()
+  }
+  // implement Update(PUT)
+  async function update() {
+    msg = "I want to update user id " + id
+  }
+  // implement Delete(DELETE)
+  async function del(sel_id) {
+    msg = "I want to delete user id " + sel_id
+  }
+  async function sel(sel_id) {
+    let p = posts.find((o) => o.id == sel_id)
+    if (p) {
+      id = p.id
+      title = p.title
+      author = p.author
+      msg = `Post ${id} selected`
+    }
+  }
+</script>
+
+<div>
+  <input placeholder="Title" bind:value={title} />
+  <input placeholder="Author" bind:value={author} />
+  <button on:click={create} disabled={!title || !author}>Create</button>
+  <button on:click={update} disabled={!title || !author || id == 0}
+    >Update {id}</button
+  >
+</div>
+{#each posts as post}
+  <div>
+    <input type="radio" name="po" on:click={() => sel(post.id)} />
+    <button on:click={() => del(post.id)}>x</button>
+    {post.title}({post.author})
+  </div>
+{/each}
+<div>{msg}</div>
---
+++ classroom/self-improvement-classroom/fetch/todo-list.svelte
@@ -0,0 +1,38 @@
+<script>
+    //code for App.svelte. Modify code to support json-server
+    // npx json-server --watch db.json
+    // https://www.w3schools.com/jsref/jsref_splice.asp
+    let newItem = ""
+    let todoList = [
+      { text: "Learn HTML", status: true },
+      { text: "Install VS Code git and Node ", status: true },
+      { text: "Create Web with html5up.net and Github page", status: true },
+      { text: "Learn Svelte", status: true },
+      { text: "Learn Web API", status: false },
+    ]
+    function addToList() {
+      todoList = [...todoList, { text: newItem, status: false }]
+      newItem = ""
+    }
+    function removeFromList(index) {
+      todoList.splice(index, 1)
+      todoList = todoList
+    }
+  </script>
+  
+  <input bind:value={newItem} type="text" placeholder="New Todo.." />
+  <button on:click={addToList}>➕</button>
+  <br />
+  {#each todoList as item, index}
+    <input bind:checked={item.status} type="checkbox" />
+    <span class:checked={item.status}>{item.text}</span>
+    <button on:click={() => removeFromList(index)}>❌</button>
+    <br />
+  {/each}
+  
+  <style>
+    .checked {
+      text-decoration: line-through;
+    }
+  </style>
+  
